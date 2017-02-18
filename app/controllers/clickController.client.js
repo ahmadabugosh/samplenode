fb5b65a3fe89e7a9a95c61ed45f604fc8874ef30
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-02-18 23:02:02 +0400

    Closed project

--- app/controllers/clickController.client.js
+++ app/controllers/clickController.client.js
@@ -5,4 +5,19 @@
    var deleteButton = document.querySelector('.btn-delete');
    var clickNbr = document.querySelector('#click-nbr');
    var apiUrl = 'http://localhost:3000/api/clicks';
+
+   function ready (fn) { ... }
+
+   function ajaxRequest (method, url, callback) {
+      var xmlhttp = new XMLHttpRequest();
+
+      xmlhttp.onreadystatechange = function () {
+         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            callback(xmlhttp.response);
+         }
+      };
+
+      xmlhttp.open(method, url, true);
+      xmlhttp.send();
+   }
 })();
